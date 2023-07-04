14061b0e790eb5593bf094960349e55d8f8ecf75
Rashmidias19 <rashmidias819@gmail.com>
2023-07-04 21:59:00 +0530

    new files were added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { jsx as _jsx } from "react/jsx-runtime";
+function Header(){
+  // return <h1>Hello World</h1>
+  return /*#__PURE__*/_jsx("h1", {
+    children: "Hello"
+  });
 }
 
-function Heading(){
-  let title="This is some heading text";
-  return(
-    <h1>title</h1>
-  );
-};
+function App() {
+  return <Header />;
+}
+
 
 export default App;
