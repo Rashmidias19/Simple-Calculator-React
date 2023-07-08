1dd7256d1a9af7fab24d4fc23d970e5aca54de5a
Rashmidias19 <rashmidias819@gmail.com>
2023-07-08 09:09:40 +0530

    changes were done

---
+++ src/Aboutme.js
@@ -0,0 +1,7 @@
+function Aboutme(){
+    return(
+        <h1>About me</h1>
+    );
+}
+
+export default Aboutme;
---
+++ src/Homepage.js
@@ -0,0 +1,8 @@
+
+function Homepage(){
+    return(
+        <h1>Welcome to my site.</h1>
+    );
+}
+
+export default Homepage;
