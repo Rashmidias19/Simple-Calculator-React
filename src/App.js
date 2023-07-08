c05cebe5c206ea0f0d802d595994de429df8892f
Rashmidias19 <rashmidias819@gmail.com>
2023-07-08 10:47:12 +0530

    video tags were added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import './App.css';
 import Homepage from './Homepage';
 import { Routes ,Route, Link } from 'react-router-dom';
 import hub from './assests/GREEN HUB2.png';
-
+import ReactPlayer from 'react-player/youtube';
 
 
 function App() {
   return(
+       
     <div className="App">
-        <img height={200} src={hub} alt="A logo icon" />
+        <MyVideo />
     </div>
+  );
+}
+
+const MyVideo = () =>{
+  return(
+    <ReactPlayer url='https://youtu.be/zZdKKzlZNyA' playing={true} volume={100} />
   );
 }
 
